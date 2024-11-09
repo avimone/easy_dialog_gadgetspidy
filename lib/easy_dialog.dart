102e4aec4d49ebb8d372a9cb6bc9ea096a65172f
Aviraj Patel <avirajpatel1resonance@gmail.com>
2024-11-09 21:38:34 +0530

    made custom changes

--- lib/easy_dialog.dart
+++ lib/easy_dialog.dart
@@ -12,6 +12,9 @@ class EasyDialog {
   final double fogOpacity;
   final double cornerRadius;
   final Color cardColor;
+  final Color color;
+  final bool isMainButton;
+  var onTap = () {};
   List<Widget> _contentList = [];
   List<Widget>? contentList;
   EdgeInsets? contentPadding;
@@ -25,11 +28,14 @@ class EasyDialog {
       this.title,
       this.description,
       this.closeButton = true,
+      this.isMainButton = false,
+      required this.onTap,
       this.height = 140,
       this.width = 300,
       this.cornerRadius = 8.0,
       this.fogOpacity = 0.37,
       this.cardColor = const Color.fromRGBO(240, 240, 240, 1.0),
+      this.color = const Color.fromRGBO(240, 240, 240, 1.0),
       this.contentList,
       this.contentPadding = const EdgeInsets.fromLTRB(17.5, 12.0, 17.5, 13.0),
       this.descriptionPadding = const EdgeInsets.all(0.0),
@@ -47,6 +53,14 @@ class EasyDialog {
         ));
   }
 
+  insertAtEnd(EdgeInsets? padding, Widget? child) {
+    _contentList.add(Container(
+      padding: padding,
+      child: child,
+      alignment: Alignment.center,
+    ));
+  }
+
   show(BuildContext context) {
     ClipRRect? image;
     if (topImage != null) {
@@ -57,7 +71,59 @@ class EasyDialog {
             topRight: Radius.circular(cornerRadius)),
       );
     }
+    Widget button = GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
 
+        // onTap();
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * .37,
+        height: 40,
+        margin: EdgeInsets.only(left: 15, right: 15),
+        /*            padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12), */
+        decoration: ShapeDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(-0.99, 0.16),
+            end: Alignment(0.99, -0.16),
+            colors: [color, color],
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        ),
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              /*   Container(
+                          width: 14,
+                          height: 14,
+                          clipBehavior: Clip.antiAlias,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("assets/images/play-6.png"),
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 8), */
+              Text(
+                'Login',
+                style: TextStyle(
+                  color: Color(0xFFF9F9FA),
+                  fontSize: 13,
+                  fontFamily: 'DM Sans',
+                  fontWeight: FontWeight.bold,
+                  height: 1.25,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
     if (title != null && description != null) {
       insertByIndex(titlePadding, title, 0);
       insertByIndex(descriptionPadding, description, 1);
@@ -72,10 +138,12 @@ class EasyDialog {
     contentList?.forEach((element) {
       insertByIndex(EdgeInsets.zero, element, _contentList.length);
     });
-
+    /* if (isMainButton) {
+      insertAtEnd(EdgeInsets.zero, button);
+    } */
     return showDialog(
       context: context,
-      barrierDismissible: true,
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return Material(
           color: Colors.transparent,
@@ -109,7 +177,7 @@ class EasyDialog {
                                   padding: contentPadding,
                                   child: Column(
                                     crossAxisAlignment: contentListAlignment,
-                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.start,
                                     children: _contentList,
                                   ),
                                 )
@@ -133,6 +201,37 @@ class EasyDialog {
                                     elevation: 2,
                                   ),
                                 )
+                              : Container(),
+                          isMainButton == true
+                              ? Positioned(
+                                  bottom: 20,
+                                  left: 0,
+                                  right: 0,
+                                  child: Container(
+                                    width:
+                                        MediaQuery.of(context).size.width * .37,
+                                    height: 40,
+                                    margin:
+                                        EdgeInsets.only(left: 25, right: 25),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                        onTap();
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: color,
+                                      ),
+                                      child: Text(
+                                        'Login',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 14,
+                                          fontFamily: 'DM Sans',
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ))
                               : Container(),
                         ],
                       ),
